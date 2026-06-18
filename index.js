2c3b2096d3412d5c03abf797dadb40f92790cdb7
kadi1m <arcanecartoon50@gmail.com>
2026-06-18 15:03:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -84,8 +84,9 @@ async function processJob(job) {
   const script = `
     if [ ! -d "${workDir}" ]; then
       echo "Repository not found locally. Cloning..."
-      mkdir -p "$(dirname "${workDir}")" || exit 1
-      git clone ${cloneUrl} ${workDir} || exit 1
+      sudo mkdir -p "$(dirname "${workDir}")" || exit 1
+      sudo chown -R $(whoami) "$(dirname "${workDir}")" || exit 1
+      git clone ${cloneUrl} "${workDir}" || exit 1
     fi
     cd ${workDir} || exit 1
     
